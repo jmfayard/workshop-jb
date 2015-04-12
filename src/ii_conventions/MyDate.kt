c10b01a48a7f5e7abad70d72461f454aca348934
jmfayard <jean-michel.fayard@smartmobilefactory.com>
2015-04-12 10:51:53 +0200

    Clever comparaison compilations
        s1 == s2
        // is compiled to
        Intrinsics.areEqual(s1, s2)
    
        b1 < b2
        // is compiled to
        b1.compareTo(b2) < 0
    
        b1 >= b2
        // is compiled to
        b1.compareTo(b2) >= 0

--- src/ii_conventions/MyDate.kt
+++ src/ii_conventions/MyDate.kt
@@ -1,6 +1,27 @@
 package ii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+import syntax.qualifiedThis.labelsForExtensionFunctionLiterals
+
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : B {
+    override fun compareTo(other: B): Int {
+        if (other !is MyDate) {
+            throw ClassCastException()
+        } else {
+            var diffYear = year - other.year
+            var diffMonth = month - other.month
+            var diffDays = dayOfMonth - other.dayOfMonth
+            if (diffYear != 0)
+                return diffYear
+            else if (diffMonth != 0)
+                return diffMonth
+            else if (diffDays != 0)
+                return diffDays
+            else
+                return 0
+        }
+    }
+
+}
 
 enum class TimeInterval {
     DAY
