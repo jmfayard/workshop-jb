f403fb504b21d3eeb58c4d3ed61b8080b3aa14be
jmfayard <jean-michel.fayard@smartmobilefactory.com>
2015-05-01 16:25:48 +0200

    Cool : declare as iterable, implement iterator with object expression

--- src/ii_conventions/MyDate.kt
+++ src/ii_conventions/MyDate.kt
@@ -1,5 +1,6 @@
 package ii_conventions
 
+import i_introduction._0_Hello_World.Hello._00_Start
 import syntax.qualifiedThis.labelsForExtensionFunctionLiterals
 
 data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : B {
@@ -20,6 +21,25 @@ data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : B {
                 return 0
         }
     }
+    fun maxDayOfTheMonth() : Int {
+        return when(month) {
+            2                       ->  28
+            1, 3, 5, 7, 8, 10, 12   ->  31
+            4, 6, 9, 11             -> 30
+            else                    -> -1
+        }
+    }
+    //fun copy(year: Int = this.year, month: Int = this.month, dayOfMonth: Int = this.dayOfMonth)  = MyDate(year, month, dayOfMonth)
+
+    fun nextDay() : MyDate {
+        if (dayOfMonth >= maxDayOfTheMonth())
+            if (month == 12)
+                return copy(year = year+1, month = 1, dayOfMonth = 1)
+            else
+                return copy(month = month+1, dayOfMonth = 1)
+        else
+                return copy(dayOfMonth = dayOfMonth+1)
+    }
 
 }
 
@@ -28,5 +48,25 @@ enum class TimeInterval {
     WEEK
     YEAR
 }
+/*
+        Make class DateRange implement Iterable<MyDate>.
+        Use object expression to implement Iterator<MyDate>.
+        Use an utility function 'MyDate.nextDay()'.
+ */
+class DateRange(public val start: MyDate, public val end: MyDate) : Iterable<MyDate> {
+    override fun iterator(): Iterator<MyDate> {
+        return object : Iterator<MyDate> {
+            var current : MyDate = start
+            override fun hasNext(): Boolean {
+                return current <= end
+            }
 
-class DateRange(public val start: MyDate, public val end: MyDate)
+            override fun next(): MyDate {
+                val result = current
+                current = current.nextDay()
+                return result
+            }
+
+        }
+    }
+}
