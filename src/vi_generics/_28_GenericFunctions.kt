933f8c8083d183334812e5ab62f11c96d904b499
jmfayard <jean-michel.fayard@smartmobilefactory.com>
2015-05-02 23:39:03 +0200

    other version

--- src/vi_generics/_28_GenericFunctions.kt
+++ src/vi_generics/_28_GenericFunctions.kt
@@ -21,25 +21,38 @@ fun task28() = TODO(
         }
 )
 
-
+fun Set<Char>.partitionLettersAndOtherSymbols(): Pair<Set<Char>, Set<Char>> {
+    //    task28()
+    return partitionTo(HashSet<Char>(), HashSet()) { c -> c in 'a'..'z' || c in 'A'..'Z' }
+}
 fun List<String>.partitionWordsAndLines(): Pair<List<String>, List<String>> {
-//    task28()
     return partitionTo(ArrayList<String>(), ArrayList()) { s -> !s.contains(" ") }
 }
 
-fun Set<Char>.partitionLettersAndOtherSymbols(): Pair<Set<Char>, Set<Char>> {
-//    task28()
-    return partitionTo(HashSet<Char>(), HashSet()) { c -> c in 'a'..'z' || c in 'A'..'Z'}
+fun <T> List<T>.partitionTo(dest1: List<T>, dest2: List<T>, predicate: (T) -> Boolean): Pair<List<T>, List<T>> {
+    return Pair(
+            dest1.plus(this.filter(predicate)),
+            dest2.plus(this.filterNot(predicate))
+    )
 }
 
-fun <T> partitionTo(c1: Collection<T>, c2: Collection<T>, predicate: (T) -> Boolean ): Pair<Ensemble<T>, Ensemble<T>> {
-    val res1 = c1.filter { predicate(it) }.toEnsemble()
-    val res2 = c1.filterNot { predicate(it) }.toEnsemble()
-    return Pair(res1, res2)
+fun <T> Set<T>.partitionTo(dest1: Set<T>, dest2: Set<T>, predicate: (T) -> Boolean): Pair<Set<T>, Set<T>> {
+    return Pair(
+            dest1.plus(this.filter(predicate)).toSet(),
+            dest2.plus(this.filterNot(predicate)).toSet()
+    )
 }
+
 
 
 public class Ensemble<T>(c: Collection<T>) : ArrayList<T>(c), Set<T> {
 
 }
 fun <T> Collection<T>.toEnsemble() : Ensemble<T> = Ensemble<T>(this)
+fun <T> partitionTo2(c1: Collection<T>, c2: Collection<T>, predicate: (T) -> Boolean ): Pair<Ensemble<T>, Ensemble<T>> {
+    val res1 = c1.filter { predicate(it) }.toEnsemble()
+    val res2 = c1.filterNot { predicate(it) }.toEnsemble()
+    return Pair(res1, res2)
+}
+
+
