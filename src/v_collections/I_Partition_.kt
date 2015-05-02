a0948cce9308b240c55df71040d24ab41f06be5c
jmfayard <jean-michel.fayard@smartmobilefactory.com>
2015-05-02 22:24:53 +0200

    RxOperators are definitely a big step forward

--- src/v_collections/I_Partition_.kt
+++ src/v_collections/I_Partition_.kt
@@ -12,5 +12,7 @@ fun example8() {
 
 fun Shop.getCustomersWithMoreUndeliveredOrdersThanDelivered(): Set<Customer> {
     // Return customers who have more undelivered orders than delivered
-    todoCollectionTask()
+    return customers.partition {
+        it.orders.filter { it.isDelivered == false }.size() >  it.orders.filter { it.isDelivered  == true}.size()
+    }.first.toSet()
 }
